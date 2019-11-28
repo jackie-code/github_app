154e43d97163b5050b6e7a493df74b0f47c29ca2
Jacqeuline Martin <paradiseprofessional88@gmail.com>
2019-11-27 20:16:19 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+//for the apikey we are using BasicAuth in the Authorization Type in Postman
+
+
+
+function watchForm() {
+    $('form').submit(event => {
+      $("#results-list").empty();
+      event.preventDefault();
+      let username = $("#js-search-term").val();
+      fetchAndDisplayRepos(username);
+    });
+  }
+  function fetchAndDisplayRepos(username) {
+    let searchURL = `https://api.github.com/users/${username}/repos`;
+    fetch(searchURL)
+      .then(response => response.json())
+      .then(responseJson => displayResults(responseJson))
+      .catch(err => {
+        $("#js-error-message").text(`Something went wrong: ${err.message}`);
+      });
+  }
+  function displayResults(responseJson) {
+    console.log(responseJson);
+    for(let i=0; i< responseJson.length; i++){
+    $("#results-list").append(`<li><a href="${responseJson[i].html_url}">Click to see repo</a></li>`);
+    $('#results').show();
+    }
+  
+  }
+  watchForm();
+  
+  
